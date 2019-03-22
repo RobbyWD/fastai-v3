7ec41d6b3726d8344f6eeae356687459c6849ecf
RobbyWD <roman.wyhowski@gmail.com>
2019-03-22 21:32:33 +0200

    new model (resnet50)

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&id=15m07_xRVTdkng0QTnXFL7c1yweEoGJcH'
-export_file_name = 'swat.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1TRLD8WUGoEHceKQBipCzTvz6Jf3vom2V'
+export_file_name = 'swat2.pkl'
 
 classes = ['armed', 'unarmed']
 path = Path(__file__).parent
